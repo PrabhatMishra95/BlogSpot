3d4196dae1f15590cada84d851b50b374cb71438
Prabhat Mishra <prabhat.m.10@gmail.com>
2018-06-29 15:31:40 +0530

    DBConfig added,POM updated.

---
+++ BlogSpotBackend/src/main/java/com/niit/config/DBConfig.java
@@ -0,0 +1,55 @@
+package com.niit.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan("com.niit")
+@EnableTransactionManagement
+public class DBConfig {
+	
+	public DataSource getDataSource() {
+		DriverManagerDataSource dataSource=new DriverManagerDataSource();
+		dataSource.setDriverClassName("");
+		dataSource.setUrl("");
+		dataSource.setUsername("");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+	
+	@Bean(name="sessionFactory")
+	public SessionFactory getSessionFactory() {
+		Properties hibernateProp=new Properties();
+		hibernateProp.put("hibernate.hbmddl2.auto","update");
+		hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.Oracle10gDialect");
+		
+		LocalSessionFactoryBuilder sessionFactoryBuilder=new LocalSessionFactoryBuilder(getDataSource());
+		sessionFactoryBuilder.addProperties(hibernateProp);
+		
+		sessionFactoryBuilder.scanPackages("com.niit");
+
+		
+		SessionFactory sessionFactory=sessionFactoryBuilder.buildSessionFactory();
+		System.out.println("Session Factory Created");
+		return sessionFactory;
+	}
+	
+	@Bean(name="txManager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory)
+	{
+		System.out.println("Transaction Manager Created");
+		return new HibernateTransactionManager(sessionFactory);
+	}
+	
+
+}
